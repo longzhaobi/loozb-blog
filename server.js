5ebba029c50fc601901695dc2bc351380c7a077d
龙召碧 <714037058@qq.com>
2017-03-28 15:22:42 +0800

    。

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({
   extended: true
