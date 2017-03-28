64aaee79aad9e96a6ab81998df24ce9958b41bf6
龙召碧 <714037058@qq.com>
2017-03-28 19:49:12 +0800

    修改文章标题颜色

--- components/article/ArticleItem.vue
+++ components/article/ArticleItem.vue
@@ -5,7 +5,7 @@
         <div class="item-heading">
           <div class="pull-right label label-warning" v-if="article.sort > 0">置顶</div>
           <div class="pull-right label label-success">{{article.classificationName}}</div>
-          <h4><a :href="'/articles/' + article.id_" class="text-muted" target="_blank">{{article.title}}</a></h4>
+          <h4><a :href="'/articles/' + article.id_" class="text-muted my-title" target="_blank">{{article.title}}</a></h4>
         </div>
         <div class="item-content">{{article.digest}}</div>
         <div class="item-footer">
@@ -38,5 +38,16 @@
   .item-heading div {
     margin-left:10px;
   }
+  .my-title {
+    color:#333333;
+    font-size:16px;
+  }
+
+  .my-title:hover, .my-title:focus, .my-title:visited
+ {
+   color: #969696 !important;
+   cursor:pointer;
+   text-decoration: none;
+ }
 
 </style>
