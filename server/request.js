e04c9fb94aa039d710d110bb9473189f0ba00b28
Long Zhao Bi <chuan_long@outlook.com>
2017-08-27 20:20:55 +0800

    新增Dockefile

--- server/request.js
+++ server/request.js
@@ -5,7 +5,7 @@ const dev = !(process.env.NODE_ENV === 'production')
 let options = {};
 console.log(dev)
 if(dev) {
-  options.baseURL = 'http://localhost:1088'
+  options.baseURL = 'http://api.loozb.com'
 } else {
   options.baseURL = 'http://api.loozb.com'
 }
